0d7c1e624c6d72a72b6ab951820ab06c9bb4904d
이준희 <qzqz08266@bodycodi.com>
2022-09-16 16:07:48 +0900

    Initial commit

---
+++ src/test/java/com/example/tech_webclient_server/TechWebclientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tech_webclient_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechWebclientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
